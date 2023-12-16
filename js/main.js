a3d4acdf029b27170296df5d0a5b805c8e10a94b
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-12-16 13:18:36 -0500

    Create operate function

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
+
+let num1 = Number(prompt("num1: "));
+let num2 = Number(prompt("num2: "));
+let operator = prompt("operator: ");
+console.log(operate(num1, num2, operator));
